9dae893ef8cbf825df26a751eab66da9785eb959
Bulat <shelbulat@mail.ru>
2021-04-07 16:39:36 +0000

    created main py for running

---
+++ main.py
@@ -0,0 +1,43 @@
+import torch
+import os
+from pathlib import Path
+from random import randint
+from typing import List
+
+import matplotlib.pyplot as plt
+import cv2
+from albumentations import (
+    Compose,
+    HorizontalFlip,
+    Normalize,
+    Resize,
+    SmallestMaxSize,
+    CenterCrop
+)
+from albumentations.pytorch import ToTensorV2
+from torch import Tensor
+from torch.utils.data import Dataset, DataLoader
+
+from src.Dataset import LandscapesDataset
+from src.hidt_model import HiDTModel
+from trainer.trainer import Trainer
+from config.hidt_config import config
+
+
+def main():
+    model = HiDTModel(config=config,
+                      device=torch.device('cuda'))
+    model.to(torch.device('cuda'))
+    path = Path('/home/jupyter/work/resources/DaytimeTranslation/data/images')
+    dataset = LandscapesDataset(path)
+    train_loader = DataLoader(dataset=dataset,
+                              batch_size=config["batch_size"],
+                              num_workers=4)
+    trainer = Trainer(model=model,
+                      config=config,
+                      train_loader=train_loader)
+#     print(trainer.optimizers, type(trainer.optimizers))
+    trainer.fit()
+    
+if __name__ == "__main__":
+    main()
